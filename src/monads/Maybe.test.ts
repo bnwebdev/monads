f3336be3d2e07efa17c876ed2fca51e4f84ffc12
Nikita Bereziuk <nikita.bereziuk@sysgears.com>
2022-12-11 23:28:31 +0200

    Initial commit

---
+++ src/monads/Maybe.test.ts
@@ -0,0 +1,58 @@
+import { Maybe } from "./Maybe";
+
+type Dog = {
+  age: number;
+  name: string;
+};
+
+type User = {
+  username: string;
+  age: number;
+  dog?: Dog;
+};
+
+const getNoUser = async (): Promise<User | null> => null;
+
+const getPartialUser = async (): Promise<User | null> => ({
+  username: "Nick",
+  age: 12,
+});
+const getFullUser = async (): Promise<User | null> => ({
+  username: "Nick2",
+  age: 23,
+  dog: { age: 1, name: "Bark" },
+});
+
+test("no user", async () => {
+  const user = new Maybe(await getNoUser());
+  const dog = user.run((user) => user.dog);
+  const name = dog.run((dog) => dog.name);
+
+  expect(user.value).toBe(null);
+  expect(dog.value).toBe(null);
+  expect(name.value).toBe(null);
+});
+
+test("partial user", async () => {
+  const user = new Maybe(await getPartialUser());
+  const dog = user.run((user) => user.dog);
+  const name = dog.run((dog) => dog.name);
+
+  expect(user.value).toEqual({ username: "Nick", age: 12 });
+  expect(dog.value).toBeUndefined();
+  expect(name.value).toBe(null);
+});
+
+test("full user", async () => {
+  const user = new Maybe(await getFullUser());
+  const dog = user.run((user) => user.dog);
+  const name = dog.run((dog) => dog.name);
+
+  expect(user.value).toEqual({
+    username: "Nick2",
+    age: 23,
+    dog: { age: 1, name: "Bark" },
+  });
+  expect(dog.value).toEqual({ age: 1, name: "Bark" });
+  expect(name.value).toBe("Bark");
+});
---
+++ src/types.ts
@@ -0,0 +1,3 @@
+export type ConstructorType<T> = new (...args: any[]) => T;
+
+export type Transformer<T, U> = (value: T) => U;
